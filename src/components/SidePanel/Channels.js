2230a04a81036d5f74473961fc722a6cabe4afe7
Gabriel Moraru <gabriel@velocitypartners.com>
2019-11-26 13:20:20 +0000

    remove listeners

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -20,6 +20,10 @@ class Channels extends Component {
         this.addListeners();
     }
 
+    componentWillUnmount() {
+        this.removeListeners();
+    }
+
     addListeners = () => {
         let loadedChannels = [];
         this.state.channelsRef.on("child_added", snap => {
@@ -28,6 +32,10 @@ class Channels extends Component {
                 this.setFirstChannel()
             );
         });
+    };
+
+    removeListeners = () => {
+        this.state.channelsRef.off();
     };
 
     setFirstChannel = () => {
