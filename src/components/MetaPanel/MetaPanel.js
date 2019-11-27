f035249f655ec589d4b4d4cc5140630724f27ac8
Gabriel Moraru <gabriel@velocitypartners.com>
2019-11-27 13:36:51 +0000

    create message collection/ replaced all files from example issue on channel files

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class MetaPanel extends Component {
-    render() {
-        return <div>MetaPanel</div>;
-    }
+class MetaPanel extends React.Component {
+  render() {
+    return <div>MetaPanel</div>;
+  }
 }
+
+export default MetaPanel;
