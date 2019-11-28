d89f138bfc0425989c24e41b9a464e9a7633ad08
Gabriel Moraru <gabriel@velocitypartners.com>
2019-11-28 12:48:49 +0000

    add upload progresss bar + fixed upload image naming

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,7 @@ import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
 
 import FileModal from "./FileModal";
+import ProgressBar from "./ProgressBar";
 
 class MessageForm extends React.Component {
     state = {
@@ -73,7 +74,7 @@ class MessageForm extends React.Component {
     uploadFile = (file, metadata) => {
         const pathToUpload = this.state.channel.id;
         const ref = this.props.messagesRef;
-        const filePath = `chat/public/${uuidv4().jpg}`;
+        const filePath = `chat/public/${uuidv4()}.jpg`;
 
         this.setState(
             {
@@ -138,7 +139,14 @@ class MessageForm extends React.Component {
             });
     };
     render() {
-        const { errors, message, loading, modal } = this.state;
+        const {
+            errors,
+            message,
+            loading,
+            modal,
+            uploadState,
+            percentUploaded
+        } = this.state;
 
         return (
             <Segment className='message__form'>
@@ -173,12 +181,16 @@ class MessageForm extends React.Component {
                         labelPosition='right'
                         icon='cloud upload'
                     />
-                    <FileModal
-                        modal={modal}
-                        closeModal={this.closeModal}
-                        uploadFile={this.uploadFile}
-                    />
                 </Button.Group>
+                <FileModal
+                    modal={modal}
+                    closeModal={this.closeModal}
+                    uploadFile={this.uploadFile}
+                />
+                <ProgressBar
+                    uploadState={uploadState}
+                    percentUploaded={percentUploaded}
+                />
             </Segment>
         );
     }
