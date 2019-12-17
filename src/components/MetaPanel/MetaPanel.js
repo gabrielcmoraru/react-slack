61a1ea522e38c9f58a26841ba530f698d16393a6
Gabriel Moraru <gabriel@velocitypartners.com>
2019-12-17 09:23:41 +0000

    display top posters in metapanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Segment, Accordion, Header, Icon, Image } from "semantic-ui-react";
+import {
+    Segment,
+    Accordion,
+    Header,
+    Icon,
+    Image,
+    List
+} from "semantic-ui-react";
 
 class MetaPanel extends React.Component {
     state = {
@@ -17,8 +24,24 @@ class MetaPanel extends React.Component {
         });
     };
 
+    displayTopPosters = posts =>
+        Object.entries(posts)
+            .sort((a, b) => b[1].count - a[1].count)
+            .map(([key, val], i) => (
+                <List.Item key={i}>
+                    <Image avatar src={val.avatar} />
+                    <List.Content>
+                        <List.Header as='a'>{key}</List.Header>
+                        <List.Description>{val.count} posts</List.Description>
+                    </List.Content>
+                </List.Item>
+            ))
+            .slice(0, 5);
+
     render() {
         const { activeIndex, privateChannel, channel } = this.state;
+        const { userPosts } = this.props;
+
         if (privateChannel) return null;
 
         return (
@@ -49,7 +72,9 @@ class MetaPanel extends React.Component {
                         Top Posters
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 1}>
-                        details
+                        <List>
+                            {userPosts && this.displayTopPosters(userPosts)}
+                        </List>
                     </Accordion.Content>
                     <Accordion.Title
                         active={activeIndex === 2}
